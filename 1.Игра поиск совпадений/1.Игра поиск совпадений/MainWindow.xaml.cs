192b084dc50e38a49ed2e78519d1d6b0b0fe6436
Nicami <Nicami>
2023-01-25 15:04:47 +0300

    1. Game MatchFounder

--- 1.Игра поиск совпадений/1.Игра поиск совпадений/MainWindow.xaml.cs
+++ 1.Игра поиск совпадений/1.Игра поиск совпадений/MainWindow.xaml.cs
@@ -15,16 +15,33 @@ using System.Windows.Shapes;
 
 namespace _1.Игра_поиск_совпадений
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthOfSecondsElapsed / 10F).ToString("0.0s");
+            if(matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text += " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -46,14 +63,55 @@ namespace _1.Игра_поиск_совпадений
             // Для каждого обхекта с типом TextBlock
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                // с помощью генератора случайных чисел выбираем индекс эмоджи из списка
-                int index = random.Next(animalEmoji.Count);
-                // находим этот эмоджи по индексу
-                string nextEmoji = animalEmoji[index];
-                // заменяем текст в блоке TextBlock изображением эмоджи
-                textBlock.Text = nextEmoji;
-                // удаляем эмоджи из списка во избежание повторного использования генератором
-                animalEmoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    // с помощью генератора случайных чисел выбираем индекс эмоджи из списка
+                    int index = random.Next(animalEmoji.Count);
+                    // находим этот эмоджи по индексу
+                    string nextEmoji = animalEmoji[index];
+                    // заменяем текст в блоке TextBlock изображением эмоджи
+                    textBlock.Text = nextEmoji;
+                    // удаляем эмоджи из списка во избежание повторного использования генератором
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+            timer.Start();
+            tenthOfSecondsElapsed = 0;
+            matchesFound= 0;
+        }
+        // Последний выбранный TextBlock
+        TextBlock lastTextBlockClicked;
+        // Искать совпадение или нет(была ли выбрана картинка ранее)
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // текущий TextBlock
+            TextBlock textBlock = sender as TextBlock;
+            if (!findingMatch)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
